62b8b48935a2116718d61172c99f7d6890b91a90
colcrunch <rhartnett35@gmail.com>
2024-05-27 06:23:11 +0000

    Add a listener for guild joins.

--- lib/listeners/welcome.ex
+++ lib/listeners/welcome.ex
@@ -9,4 +9,16 @@ defmodule Eris.Listeners.WelcomeListener do
       _ -> :noop
     end
   end
+
+  def handle_event({:GUILD_MEMBER_ADD, member, _}) do
+    guild = elem(member, 0)
+    user = elem(member, 1)
+    name =
+      case user.nick do
+      nil -> elem(Nostrum.Api.get_user(user.user_id), 1).username
+      _ -> user.nick
+      end
+    Logger.debug("#{name} joined #{guild}")
+    IO.inspect(member)
+  end
 end
